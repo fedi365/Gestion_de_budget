1b10990bd03f84f7a17e70c2778d9685f351db68
Fedi Mejri <fedi.mejri@dh-ss.com>
2026-01-29 13:41:57 +0000

    correct problem of build v3

--- src/main/java/org/example/gestion_de_budget/model/CATEGORY.java
+++ src/main/java/org/example/gestion_de_budget/model/CATEGORY.java
@@ -5,18 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "categories")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,4 +19,59 @@ public class Category {
     private BigDecimal monthlyLimit;
     private TransactionType type;
 
+    // Constructeur par défaut
+    public Category() {
+    }
+
+    // Constructeur avec tous les champs
+    public Category(Long id, String name, BigDecimal monthlyLimit, TransactionType type) {
+        this.id = id;
+        this.name = name;
+        this.monthlyLimit = monthlyLimit;
+        this.type = type;
+    }
+
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getMonthlyLimit() {
+        return monthlyLimit;
+    }
+
+    public void setMonthlyLimit(BigDecimal monthlyLimit) {
+        this.monthlyLimit = monthlyLimit;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", monthlyLimit=" + monthlyLimit +
+                ", type=" + type +
+                '}';
+    }
 }
